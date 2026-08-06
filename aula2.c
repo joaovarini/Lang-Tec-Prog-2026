a11f437d315fff6f6492f7d087624f4e7be885b1
joaovarini <joaovitorvarini713@gmail.com>
2026-08-05 22:27:13 -0300

    Add files via upload

---
+++ aula2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+
+	int a,b,c;
+	float resultado;
+	a = 15;
+	b = 29;
+	c = 10;
+	
+	resultado = (a+b+c)/3;
+	
+	
+	printf(" A media desses tres varores %d, %d e %d = %.2f ", a,b,c, resultado);
+	
+	return 0;
+	
+}
